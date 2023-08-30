084ec6fba9abe1cc3869e7164897fef1e7f9c6b0
nfillip <nick.m.fillip@gmail.com>
2023-08-30 00:10:50 -0600

    cleaned up code with comments and added a prompt

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,36 @@
-//GLOBAL VERIABLES
-var score = 0;
-var secondsLeft = 60;
-//pulling from HTML ids
+//:::GLOBAL VERIABLES
+////start page
 var headerLink = document.querySelector("#topHead");
 var headerTimer = document.querySelector("#timer");
 var startButton = document.querySelector("#start-button");
 var startPage = document.querySelector("#start-page");
+////quiz page
 var quizSection = document.querySelector("#quiz-section");
-var quizChoices = document.querySelector("#div-quiz");
-var resultsSection = document.querySelector("#results");
-var pointsFinal = document.querySelector("#points");
 var questionTitle = document.querySelector("#question-title");
+var quizChoices = document.querySelector("#div-quiz");
 var buttonA = document.querySelector("#buttonA");
 var buttonB = document.querySelector("#buttonB");
 var buttonC = document.querySelector("#buttonC");
 var choiceA = document.querySelector("#choiceA");
 var choiceB = document.querySelector("#choiceB");
 var choiceC = document.querySelector("#choiceC");
-var submitButton = document.querySelector("#submit-score");
+////quiz page - (reassurance - saying if it was correct or not)
 var tag = document.createElement("h2");
 document.body.appendChild(tag);
 var answerReassurance = document.querySelector("h2");
+////results page
+var resultsSection = document.querySelector("#results");
+var pointsFinal = document.querySelector("#points");
+var submitButton = document.querySelector("#submit-score");
 var yourInitials = document.querySelector("#initials");
+////highscores page
 var highScoreTable;
 var finalSection = document.querySelector("#highscore");
-//quiz answerKey array
+var finalDiv = document.querySelector("#finalDiv");
+var resetButton = document.querySelector("#reset-button");
+////timer Variables
+var secondsLeft = prompt("How many seconds do you want on the quiz?");
+////quiz answerKey array
 var finalScore;
 var i = 0;
 var j = 0;
@@ -49,10 +55,20 @@ var answerKey = [
     choice: ["adds 2", "adds 1", "undefined"],
     answer: "adds 1",
   },
+  {
+    title: "local storage can only store a:",
+    choice: ["object", "array", "string"],
+    answer: "string",
+  },
+  {
+    title: "DOM stands for?",
+    choice: ["Dominican Republic", "Domestic violence", "Document Object Model"],
+    answer: "Document Object Model",
+  },
 ];
 
 //:::FUNCTIONS
-//displayQuiz: displays first question of quiz and turns start page display to none
+//displayQuiz: (displays first question of quiz and turns start page display to none)
 function displayQuiz() {
   setTime();
   startPage.setAttribute("style", "display: none");
@@ -69,11 +85,9 @@ function displayQuiz() {
   choiceA.textContent = answerKey[0].choice[0];
   choiceB.textContent = answerKey[0].choice[1];
   choiceC.textContent = answerKey[0].choice[2];
-
-  //start timer
 }
 
-//displayResults: displays result screen with your score after the quiz
+//displayResults: (displays result screen with your score after the quiz)
 function displayResults() {
   quizSection.setAttribute("style", "display: none");
   resultsSection.setAttribute(
@@ -83,54 +97,63 @@ function displayResults() {
   pointsFinal.textContent = "Your Final Point Total: " + finalScore;
 }
 
-
-
-//testAnswer: compare answer choice with the correct answer in the answer object
+//testAnswer: (compare answer choice with the correct answer in the answerKey object)
 function testAnswer(answerChoice) {
   if (i === answerKey.length - 1) {
     finalScore = secondsLeft;
     j = 1;
     if (answerChoice !== answerKey[i].answer) {
+      //incorrect final answer choice
       finalScore -= 10;
-      j = 1;
+      if (finalScore < 0) {
+        finalScore = 0;
+      }
+      answerReassurance.setAttribute("style", "color: red; font-size: 2em");
       answerReassurance.textContent = "That was incorrect.";
-    var h2Timer = setInterval(function () {
-      answerReassurance.textContent = "";
-    }, 3000);
+      var h2Timer = setInterval(function () {
+        answerReassurance.textContent = "";
+      }, 3000);
     } else {
-      j = 1;
+      //correct final answerchoice
+      answerReassurance.setAttribute("style", "color: green; font-size: 2em");
       answerReassurance.textContent = "That was correct.";
-    var h2Timer = setInterval(function () {
-      answerReassurance.textContent = "";
-    }, 3000);
+      var h2Timer = setInterval(function () {
+        answerReassurance.textContent = "";
+      }, 3000);
     }
-    displayResults();
-    //go to the results page
+    displayResults(); //go to the results page
   } else if (answerChoice === answerKey[i].answer) {
+    //correct answer but not last question
     i++;
-    score++;
-
     questionTitle.textContent = answerKey[i].title;
     choiceA.textContent = answerKey[i].choice[0];
     choiceB.textContent = answerKey[i].choice[1];
     choiceC.textContent = answerKey[i].choice[2];
+    answerReassurance.setAttribute("style", "color: green; font-size: 2em");
     answerReassurance.textContent =
       "That was correct! Here's the next question...";
     var h2Timer = setInterval(function () {
       answerReassurance.textContent = "";
     }, 3000);
   } else {
+    //incorrect answer but not last question
     i++;
     secondsLeft -= 10;
     questionTitle.textContent = answerKey[i].title;
     choiceA.textContent = answerKey[i].choice[0];
     choiceB.textContent = answerKey[i].choice[1];
     choiceC.textContent = answerKey[i].choice[2];
-    answerReassurance.textContent = "That was incorrect. Here's the next question...";
+    if (secondsLeft > 0) {
+      answerReassurance.setAttribute("style", "color: red; font-size: 2em");
+      answerReassurance.textContent =
+        "That was incorrect. Here's the next question...";
+    } else {
+      answerReassurance.setAttribute("style", "color: red; font-size: 2em");
+      answerReassurance.textContent = "That was incorrect. GAMEOVER.";
+    }
     var h2Timer = setInterval(function () {
       answerReassurance.textContent = "";
     }, 3000);
-    
   }
 }
 
@@ -140,121 +163,99 @@ function setTime() {
     secondsLeft--;
     headerTimer.setAttribute("style", "font-size: 3rem;");
     headerTimer.textContent = secondsLeft;
-    if (secondsLeft === 0 || j === 1) {
+    if (secondsLeft <= 0) {
+      //ends the timer on a LOSS when we drop to 0 points or less. Calls results page.
       clearInterval(timerInterval);
-      headerTimer.textContent = "Timer:"
+      headerTimer.textContent = "Timer:";
+      headerTimer.setAttribute("style", "font-size: 1.5rem;");
+      finalScore = 0;
+      displayResults();
+    } else if (j === 1) {
+      //ends the timer when we complete the final question
+      clearInterval(timerInterval);
+      headerTimer.textContent = "Timer:";
       headerTimer.setAttribute("style", "font-size: 1.5rem;");
     }
   }, 1000);
 }
 
-
 //function: addtoLocal - takes the input value from the input element and stores locally
 function addToLocal() {
-    
-    var mostRecent = {
-        initials: yourInitials.value,
-        score: finalScore 
-    }
-    if(localStorage.getItem("initial") === null){
-        localStorage.setItem("initial", JSON.stringify([mostRecent]));
-        
-    } else {
-        var temp = JSON.parse(localStorage.getItem("initial"));
-        temp.push(mostRecent);
-        
-        localStorage.setItem("initial" , JSON.stringify(temp));
-        
-    }
-    // goToResultsPage
-    // localStorage.setItem("initial", x)
+  var mostRecent = {
+    initials: yourInitials.value,
+    score: finalScore,
+  };
+  if (localStorage.getItem("initial") === null) {
+    localStorage.setItem("initial", JSON.stringify([mostRecent]));
+  } else {
+    var temp = JSON.parse(localStorage.getItem("initial"));
+    temp.push(mostRecent);
+    localStorage.setItem("initial", JSON.stringify(temp));
+  }
+  addToHighScorePage();
 }
 
+//function: addtoHighScorePage - sets other pages to display none, calls teh sort object array, takes localStorage and displays it by building List Items
 function addToHighScorePage() {
-    finalSection.setAttribute("style", "display: grid; grid-template-columns: 1fr 1fr");
-    resultsSection.setAttribute("style", "display: none");
-    quizSection.setAttribute("style", "display: none");
-    startPage.setAttribute("style", "display: none");
-    headerLink.textContent = "retake the quiz"
-    highScoreTable = JSON.parse(localStorage.getItem("initial"));
-    // console.log(highScoreTable);
-    
-    //sort the array
-    var array2 = [1,5,7,3,6,4];
-    var temp2;
-    var largest;
-    var indexLargestInitial = -1;
-   
-    for(var j = 0; j<array2.length-1; j++) {
-        largest = array2[j];
-            // console.log("largest: " + largest);
-        indexLargestInitial = -1;
-        for (var h = j+1; h<array2.length; h++) {
-            if (array2[h] > largest) {
-            largest = array2[h];
-            // console.log("largest: " + largest);
-            indexLargestInitial = h;
-            // console.log("largestInitiral: " + h);
-            } 
-        }
-        if (indexLargestInitial !== -1){
-        temp2 = array2[j];
-        // console.log("temp: " + temp2)
-        array2[j] = largest;
-        // console.log("array2[j]: " + array2[j]);
-        array2[indexLargestInitial] = temp2;
-        // console.log("array2[h]: " + array2[indexLargestInitial]);
-         
-        console.log(array2);
-        }
-        
-    }
-    console.log(array2);
+  finalSection.setAttribute(
+    "style",
+    "display: flex; flex-direction: column; align-items: center; jusify-content: space-around"
+  );
+  finalDiv.setAttribute(
+    "style",
+    "display: grid; grid-template-columns: 1fr 1fr"
+  );
+  resetButton.setAttribute("style", "margin-top: 2em");
 
-    for(var j = 0; j<highScoreTable.length-1; j++) {
-        largest = highScoreTable[j].score;
-        largestObject = highScoreTable[j];
-            // console.log("largest: " + largest);
-        indexLargestInitial = -1;
-        for (var h = j+1; h<highScoreTable.length; h++) {
-            if (highScoreTable[h].score > largest) {
-            largest = highScoreTable[h].score;
-            largestObject = highScoreTable[h];
-            // console.log("largest: " + largest);
-            indexLargestInitial = h;
-            // console.log("largestInitiral: " + h);
-            } 
-        }
-        if (indexLargestInitial !== -1){
-        temp2 = highScoreTable[j];
-        // console.log("temp: " + temp2)
-        highScoreTable[j] = largestObject;
-        // console.log("array2[j]: " + array2[j]);
-        highScoreTable[indexLargestInitial] = temp2;
-        // console.log("array2[h]: " + array2[indexLargestInitial]);
-         
-        console.log(highScoreTable);
-        }
-        
-    }
-    console.log(array2);
+  resultsSection.setAttribute("style", "display: none");
+  quizSection.setAttribute("style", "display: none");
+  startPage.setAttribute("style", "display: none");
+  headerLink.textContent = "retake the quiz";
+  highScoreTable = JSON.parse(localStorage.getItem("initial"));
+  sortArrayofObjects();
 
-    for (var h = 0; h<highScoreTable.length; h++) {
-        
-        var tag1 = document.createElement("li");
-        var tag2 = document.createElement("li");
-        finalSection.appendChild(tag1);
-        finalSection.appendChild(tag2);
-        tag1.textContent = highScoreTable[h].initials;
-        tag2.textContent = highScoreTable[h].score;
-        
+  for (var h = 0; h < highScoreTable.length; h++) {
+    var tag1 = document.createElement("li");
+    var tag2 = document.createElement("li");
+    finalDiv.appendChild(tag1);
+    finalDiv.appendChild(tag2);
+    tag1.textContent = highScoreTable[h].initials;
+    tag2.textContent = highScoreTable[h].score;
+  }
+}
+
+//function sortArrayofObjects: puts the top score with respective initial at top of high scoers page
+function sortArrayofObjects() {
+  for (var j = 0; j < highScoreTable.length - 1; j++) {
+    largest = highScoreTable[j].score;
+    largestObject = highScoreTable[j];
+    // console.log("largest: " + largest);
+    indexLargestInitial = -1;
+    for (var h = j + 1; h < highScoreTable.length; h++) {
+      if (highScoreTable[h].score > largest) {
+        largest = highScoreTable[h].score;
+        largestObject = highScoreTable[h];
+        // console.log("largest: " + largest);
+        indexLargestInitial = h;
+        // console.log("largestInitiral: " + h);
+      }
     }
+    if (indexLargestInitial !== -1) {
+      temp2 = highScoreTable[j];
+      // console.log("temp: " + temp2)
+      highScoreTable[j] = largestObject;
+      // console.log("array2[j]: " + array2[j]);
+      highScoreTable[indexLargestInitial] = temp2;
+      // console.log("array2[h]: " + array2[indexLargestInitial]);
+    }
+  }
 }
 
 //:::EVENT LISTENERS
+////start button
 startButton.addEventListener("click", displayQuiz);
 
-//when quiz button is clicked, I want to use textContent to change what is on each p value. I want to check if the button choice was equal to the answer key on the answer variable. I want to increment index by 1 of the answer key.
+////buttonA
 buttonA.addEventListener("click", function (event) {
   event.stopPropagation();
   var element = event.target;
@@ -262,7 +263,7 @@ buttonA.addEventListener("click", function (event) {
     testAnswer(choiceA.textContent);
   }
 });
-
+////buttonB
 buttonB.addEventListener("click", function (event) {
   event.stopPropagation();
   var element = event.target;
@@ -270,7 +271,7 @@ buttonB.addEventListener("click", function (event) {
     testAnswer(choiceB.textContent);
   }
 });
-
+////buttonC
 buttonC.addEventListener("click", function (event) {
   event.stopPropagation();
   var element = event.target;
@@ -279,38 +280,40 @@ buttonC.addEventListener("click", function (event) {
   }
 });
 
+////submitButton
+submitButton.addEventListener("click", function (event) {
+  event.stopPropagation();
+  event.preventDefault();
+  var element = event.target;
+  if (element.matches("button")) {
+    addToLocal();
+  }
+  //   addToHighScorePage();
+});
 
-submitButton.addEventListener("click", function (event){
-    event.stopPropagation();
-    event.preventDefault();
-    var element = event.target;
-    if (element.matches("button")){
-        addToLocal();
+////headerLink - takes you to highscores page
+headerLink.addEventListener("click", function (event) {
+  event.stopPropagation();
+  event.preventDefault();
+  var element = event.target;
+  if (element.matches("div")) {
+    if (headerLink.textContent === "Go To HighScores") {
+      j = 1;
+      addToHighScorePage();
+      headerTimer.textContent = "Timer:";
+      headerTimer.setAttribute("style", "font-size: 1.5rem");
+    } else {
+      location.reload();
     }
-    
-    addToHighScorePage();
- 
-}
-)
+  }
+});
 
-headerLink.addEventListener("click", function(event) {
-    
-    event.stopPropagation();
-    event.preventDefault();
-    var element = event.target;
-    if (element.matches("div")){
-       if (headerLink.textContent === "Go To HighScores"){
-            j = 1 ;
-            addToHighScorePage();
-            headerTimer.textContent = "Timer:"
-            headerTimer.setAttribute("style", "font-size: 1.5rem")
-       } else {
-            location.reload();
-       }
-    }
-})
-
-if (secondsLeft <=0) {
-    finalScore = 0;
-    displayResults();
-}
+resetButton.addEventListener("click", function (event) {
+  event.stopPropagation();
+  event.preventDefault();
+  var element = event.target;
+  if (element.matches("button")) {
+    localStorage.removeItem("initial");
+    location.reload();
+  }
+});
